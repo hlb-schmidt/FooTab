fe977808a30bf6b14e0a1f3df8ab64f1d8bebb1d
Andre Schmidt <schmidt@holzlandbecker.de>
2018-03-29 13:24:48 +0200

    add more tests

--- Controllers/Frontend/TestFooPersist.php
+++ Controllers/Frontend/TestFooPersist.php
@@ -6,12 +6,30 @@ class Shopware_Controllers_Frontend_TestFooPersist extends Enlight_Controller_Ac
     public function indexAction ()
     {
         $em = $this->getModelManager();
+        $filter = $em->find("Shopware\Models\Property\Option", 1138); // sorry, this if for our data. not demo data...
 
+        // addFilter() WORKS
         $tab = new Tab();
-        $tab->setName("test");
-        $filter = $em->find("Shopware\Models\Property\Option", 1046); // sorry, this if for our data. not demo data...
+        $tab->setName("test1");
         $tab->addFilter($filter);
+        $em->persist($tab);
+        $em->flush();
 
+        // setFilters() with copy WORKS
+        $tab = new Tab();
+        $tab->setName("test2");
+        $filters = $tab->getFilters();
+        $filters->add($filter);
+        $tab->setFilters($filters);
+        $em->persist($tab);
+        $em->flush();
+
+        // TODO: setFilters() with new does NOT WORK
+        $tab = new Tab();
+        $tab->setName("test3");
+        $filters = new \Doctrine\Common\Collections\ArrayCollection();
+        $filters->add($filter);
+        $tab->setFilters($filters);
         $em->persist($tab);
         $em->flush();
 
